4bbf922ff55c031a0ef3326a693b2a7fbd0f64ff
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-10-28 20:33:49 +0530

    ADD - CSE machine single control exec

--- src/main/java/nipunayf/rpalinterpreter/csemachine/Machine.java
+++ src/main/java/nipunayf/rpalinterpreter/csemachine/Machine.java
@@ -1,5 +1,7 @@
 package nipunayf.rpalinterpreter.csemachine;
 
+import nipunayf.rpalinterpreter.SymbolDictionary;
+import nipunayf.rpalinterpreter.csemachine.environment.Environment;
 import nipunayf.rpalinterpreter.tree.node.Node;
 
 import java.util.HashMap;
@@ -13,17 +15,17 @@ public class Machine {
     /**
      * Operands are temporarily kept in this stack until a function evaluates them.
      */
-    private static Stack<Node> stack;
+    static Stack<Node> stack;
 
     /**
      * Applicative expression is divided into the control stacks based on lambda definitions.
      */
-    private static HashMap<Integer, List<Node>> control;
+    static Stack<Node> control;
 
     /**
      * The current environment of the CSE machine
      */
-    private static int currentEnvironment;
+    static Environment currentEnvironment;
 
 
 
@@ -38,7 +40,27 @@ public class Machine {
     /**
      * Evaluates the current control structures.
      */
-    public static void evaluate() {
+    public static String evaluate() throws InvalidCSEMachineException, NoSuchMethodException {
+        // Iterating until the control stack is empty
+        while(!control.empty()) {
+            Node node = control.pop();
 
+            // Substitute the identifier with the appropriate data node
+            if (node.getType() == SymbolDictionary.Symbol.IDENTIFIER) {
+                stack.push(currentEnvironment.construe(node));
+            }
+
+            // Node is a data node. Hence, adding to the stack
+            else if (node.getType() != SymbolDictionary.Symbol.OPERATOR) {
+                stack.push(node);
+            }
+
+            // Node is an operator.
+            else {
+                node.execute(stack);
+            }
+        }
+
+        return stack.pop().getValue();
     }
 }
--- src/test/java/nipunayf/rpalinterpreter/csemachine/MachineTest.java
+++ src/test/java/nipunayf/rpalinterpreter/csemachine/MachineTest.java
@@ -1,6 +1,13 @@
 package nipunayf.rpalinterpreter.csemachine;
 
+import nipunayf.rpalinterpreter.SymbolDictionary;
+import nipunayf.rpalinterpreter.csemachine.environment.PreliminaryEnvironment;
+import nipunayf.rpalinterpreter.tree.node.DataNode;
+import nipunayf.rpalinterpreter.tree.node.operators.NegNode;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +15,40 @@ class MachineTest {
 
     @Test
     void shouldEvaluateSingleControl() {
+        Machine.stack = new Stack<>();
+        Machine.currentEnvironment = new PreliminaryEnvironment(new HashMap<>());
+        Machine.control = new Stack<>() {{
+            push(new NegNode(0, "neg", SymbolDictionary.Symbol.OPERATOR));
+            push(new NegNode(1, "neg", SymbolDictionary.Symbol.OPERATOR));
+            push(new NegNode(2, "neg", SymbolDictionary.Symbol.OPERATOR));
+            push(new DataNode(3, "1", SymbolDictionary.Symbol.INTEGER));
+        }};
 
+        try {
+            assertEquals("-1", Machine.evaluate());
+        } catch (InvalidCSEMachineException | NoSuchMethodException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void shouldCheckTheEnvironmentForIdentifiers() {
+        DataNode identifier = new DataNode(3, "x", SymbolDictionary.Symbol.IDENTIFIER);
+        Machine.stack = new Stack<>();
+        Machine.currentEnvironment = new PreliminaryEnvironment(new HashMap<>() {{
+            put(identifier, new DataNode(3, "1", SymbolDictionary.Symbol.INTEGER));
+        }});
+        Machine.control = new Stack<>() {{
+            push(new NegNode(0, "neg", SymbolDictionary.Symbol.OPERATOR));
+            push(new NegNode(1, "neg", SymbolDictionary.Symbol.OPERATOR));
+            push(new NegNode(2, "neg", SymbolDictionary.Symbol.OPERATOR));
+            push(identifier);
+        }};
+
+        try {
+            assertEquals("-1", Machine.evaluate());
+        } catch (InvalidCSEMachineException | NoSuchMethodException e) {
+            fail(e.getMessage());
+        }
     }
 }
