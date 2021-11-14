5df977af9cbd4d08578718ddc6612e77b93c9870
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-11-15 00:28:30 +0530

    MOD - universal op to support all data types

--- src/main/java/nipunayf/rpalinterpreter/tree/node/operators/UniversalOpNode.java
+++ src/main/java/nipunayf/rpalinterpreter/tree/node/operators/UniversalOpNode.java
@@ -7,8 +7,13 @@ import nipunayf.rpalinterpreter.tree.node.DataNode;
 import nipunayf.rpalinterpreter.tree.node.Node;
 import nipunayf.rpalinterpreter.tree.node.OperatorNode;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Stack;
 
+/**
+ * This class contains the set of all operators that works on any data types
+ */
 public class UniversalOpNode extends OperatorNode {
     /**
      * Creates an operator node
@@ -26,17 +31,29 @@ public class UniversalOpNode extends OperatorNode {
         Node firstData = stack.pop();
         Node secondData = stack.pop();
 
-        if (firstData.getType() != secondData.getType())
-            throw new InvalidCSEMachineException("Both operands should be of same type");
-
         boolean value;
         switch (OperatorDictionary.map.get(this.getValue())) {
             case EQUAL:
-                value = checkEquality(firstData, secondData);
+                if (firstData.getType() == secondData.getType()) {
+                    value = firstData.getType() != DataDictionary.Data.INTEGER &&
+                            firstData.getType() != DataDictionary.Data.STRING &&
+                            firstData.getType() != DataDictionary.Data.BOOLEAN &&
+                            firstData.getType() != DataDictionary.Data.OPERATOR ||
+                            checkEquality(firstData, secondData);
+                } else {
+                    value = false;
+                }
                 break;
 
             case NOT_EQUAL:
-                value = checkInequality(firstData, secondData);
+                if (firstData.getType() == secondData.getType()) {
+                    value = (firstData.getType() == DataDictionary.Data.INTEGER ||
+                            firstData.getType() == DataDictionary.Data.STRING ||
+                            firstData.getType() == DataDictionary.Data.BOOLEAN ||
+                            firstData.getType() == DataDictionary.Data.OPERATOR) && checkInequality(firstData, secondData);
+                } else {
+                    value = true;
+                }
                 break;
 
             default:
@@ -46,29 +63,81 @@ public class UniversalOpNode extends OperatorNode {
         stack.push(new DataNode(firstData.getLevel(), Boolean.toString(value), DataDictionary.Data.BOOLEAN));
     }
 
-    private boolean checkEquality(Node firstOperand, Node secondOperand) throws InvalidCSEMachineException {
-        switch (firstOperand.getType()) {
-            case INTEGER:
-                return (Integer.parseInt(firstOperand.getValue()) == Integer.parseInt(secondOperand.getValue()));
-            case STRING:
-                return (firstOperand.getValue().equals(secondOperand.getValue()));
-            case BOOLEAN:
-                return (Boolean.parseBoolean(firstOperand.getValue()) == Boolean.parseBoolean(secondOperand.getValue()));
-            default:
-                throw new InvalidCSEMachineException("Invalid operator " + this.getValue() + " for integers");
+    /**
+     * Check if the two elements are equal
+     *
+     * @param firstOperand  first element of the stack
+     * @param secondOperand second element of the stack
+     * @return true if elements are equal
+     * @throws InvalidCSEMachineException if the elements are not of the supported types
+     */
+    private boolean checkEquality(Node firstOperand, Node secondOperand) throws InvalidCSEMachineException, NoSuchMethodException {
+        if (OperatorDictionary.map.get(firstOperand.getValue()) == OperatorDictionary.Operator.TAU &&
+                OperatorDictionary.map.get(secondOperand.getValue()) == OperatorDictionary.Operator.TAU) {
+            return checkTupleEquality(firstOperand, secondOperand);
+        } else {
+            switch (firstOperand.getType()) {
+                case INTEGER:
+                    return (Integer.parseInt(firstOperand.getValue()) == Integer.parseInt(secondOperand.getValue()));
+                case STRING:
+                    return (firstOperand.getValue().equals(secondOperand.getValue()));
+                case BOOLEAN:
+                    return (Boolean.parseBoolean(firstOperand.getValue()) == Boolean.parseBoolean(secondOperand.getValue()));
+                default:
+                    return false;
+            }
+
         }
     }
 
-    private boolean checkInequality(Node firstOperand, Node secondOperand) throws InvalidCSEMachineException {
-        switch (firstOperand.getType()) {
-            case INTEGER:
-                return (Integer.parseInt(firstOperand.getValue()) != Integer.parseInt(secondOperand.getValue()));
-            case STRING:
-                return (!firstOperand.getValue().equals(secondOperand.getValue()));
-            case BOOLEAN:
-                return (Boolean.parseBoolean(firstOperand.getValue()) != Boolean.parseBoolean(secondOperand.getValue()));
-            default:
-                throw new InvalidCSEMachineException("Invalid operator " + this.getValue() + " for integers");
+    /**
+     * Check if the two elements are not equal
+     *
+     * @param firstOperand  first element of the stack
+     * @param secondOperand second element of the stack
+     * @return true if elements are not equal
+     * @throws InvalidCSEMachineException if the elements are not of the supported types
+     */
+    private boolean checkInequality(Node firstOperand, Node secondOperand) throws InvalidCSEMachineException, NoSuchMethodException {
+        if (OperatorDictionary.map.get(firstOperand.getValue()) == OperatorDictionary.Operator.TAU &&
+                OperatorDictionary.map.get(secondOperand.getValue()) == OperatorDictionary.Operator.TAU) {
+            return !checkTupleEquality(firstOperand, secondOperand);
+        } else {
+            switch (firstOperand.getType()) {
+                case INTEGER:
+                    return (Integer.parseInt(firstOperand.getValue()) != Integer.parseInt(secondOperand.getValue()));
+                case STRING:
+                    return (!firstOperand.getValue().equals(secondOperand.getValue()));
+                case BOOLEAN:
+                    return (Boolean.parseBoolean(firstOperand.getValue()) != Boolean.parseBoolean(secondOperand.getValue()));
+                default:
+                    return true;
+            }
+        }
+
+    }
+
+    private boolean checkTupleEquality(Node firstOperand, Node secondOperand) throws NoSuchMethodException {
+        List<Node> firstChildren = firstOperand.getChildren();
+        List<Node> secondChildren = secondOperand.getChildren();
+
+        if (firstChildren.size() == secondChildren.size()) {
+            int numChildren = firstChildren.size();
+
+            Node firstChild, secondChild;
+            for (int i=0; i<numChildren; i++) {
+                firstChild = firstChildren.get(i);
+                secondChild = secondChildren.get(i);
+
+                if (firstChild.getType() ==  secondChild.getType()) {
+                    if (!Objects.equals(firstChild.getValue(), secondChild.getValue())) return false;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
         }
     }
 }
